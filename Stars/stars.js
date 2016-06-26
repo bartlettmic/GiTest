eb1f142c1cc36d7556e28c38b04e4cafdb373a58
bartlettmic <bartlettmic@gmail.com>
2016-06-25 21:26:07 -0600

    Improved checkbox styling (text triggers change, actual box not visible) Added background configuration (black or white currently)

--- Stars/stars.js
+++ Stars/stars.js
@@ -38,14 +38,6 @@ $(document).ready(function() {
     this.checked = window[this.id] ? window[this.id].toString() : "";
   });
   for (var i = 0; i < stars; i++) dots.push(new Dot(i));
-  var ua = navigator.userAgent;
-  var isiPad = /iPad/i.test(ua) || /iPhone OS 3_1_2/i.test(ua) || /iPhone OS 3_2_2/i.test(ua);
-  if (isiPad){
-    var find = 'javascript:;';
-    var re = new RegExp(find, 'g');
-
-    document.body.innerHTML = document.body.innerHTML.replace(re, 'javascript:void(0);');
-  }
 });
 
 //update mouse position
